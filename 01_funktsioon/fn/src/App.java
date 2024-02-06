f935c6b1d8855bedd7ae91e986767721ccb8e557
MRmikimous <germotael@gmail.com>
2024-02-06 15:28:18 +0200

    save

--- 01_funktsioon/fn/src/App.java
+++ 01_funktsioon/fn/src/App.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(hello("World"));
+        System.out.println(hello("Dog"));
+
+        System.out.println(rand(0, 100));
+        System.out.println(rand(3, 55));
+
+        System.out.println(kaibeMaksHinnast(2.3));
+        System.out.println(kaibeMaksHinnast(74.32));
+    }
+
+    private static String hello(String name) {
+        return "Hello, " + name + "!";
+    }
+
+    private static int rand(int min, int max) {
+        return new Random().nextInt(min, max);
+    }
+
+    private static double kaibeMaksHinnast(double price) {
+        return price * 0.2;
     }
 }
