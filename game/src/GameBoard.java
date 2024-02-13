4be405bb4ec6240e3aeb708a3708273689778e3a
MRmikimous <germotael@gmail.com>
2024-02-13 16:20:29 +0200

    save

--- game/src/GameBoard.java
+++ game/src/GameBoard.java
@@ -13,17 +13,39 @@ public class GameBoard {
         this._board = board;
     }
 
+    /*
+     * 1, ─ 2, │ 3, ┎ 4, ┒ 5, ┕ 6, ┛ 7, ┿
+     */
     public String stringify(Creature[] creatures) {
         StringBuilder sb = new StringBuilder();
         for (int y = 0; y < _board.length; y++) {
             for (int x = 0; x < _board[y].length; x++) {
                 char symbol = ' ';
 
-                if (y == 0 || y == _board.length - 1) {
-                    symbol = '-';
-                } else if (x == 0 || x == _board[y].length - 1) {
-                    symbol = '|';
+                if (_board[y][x] == 1) {
+                    symbol = '─';
+                } else if (_board[y][x] == 2) {
+                    symbol = '│';
+                } else if (_board[y][x] == 3) {
+                    symbol = '┌';
+                } else if (_board[y][x] == 4) {
+                    symbol = '┐';
+                } else if (_board[y][x] == 5) {
+                    symbol = '└';
+                } else if (_board[y][x] == 6) {
+                    symbol = '┘';
+                } else if (_board[y][x] == 7) {
+                    symbol = '┤';
+                } else if (_board[y][x] == 8) {
+                    symbol = '┴';
+                } else if (_board[y][x] == 9) {
+                    symbol = '├';
                 }
+                // if (y == 0 || y == _board.length - 1) {
+                // symbol = '-';
+                // } else if (x == 0 || x == _board[y].length - 1) {
+                // symbol = '|';
+                // }
                 for (Creature creature : creatures) {
                     if (x == creature.location().x
                             && y == creature.location().y) {
